363a922d1a0c970f487605dc31c178d537660ec6
dj-backups <plopydasgaming@gmail.com>
2025-07-03 12:54:56 +0100

    create run_experiments batch script to automate testing workflows

---
+++ _examples/original/_run_experiments.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+set ITERATIONS=20
+
+REM first run just to get some history
+python dagforpredictions_expensive_computations.py simple
+
+REM Actually run the loops
+for /L %%i in (1,1,%ITERATIONS%) do (
+    echo Running first iteration %%i
+    python dagforpredictions_expensive_computations.py simple
+)
+
+for /L %%j in (1,1,%ITERATIONS%) do (
+    echo Running second iteration %%j
+    python dagforpredictions_expensive_computations.py first
+)
+
+echo All runs completed.
+pause
+exit /b
