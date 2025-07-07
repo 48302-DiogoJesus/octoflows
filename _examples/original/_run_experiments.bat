8023341614059e67f58d7044ea4158f9b4b62c63
dj-backups <plopydasgaming@gmail.com>
2025-07-07 18:45:16 +0100

    ran 20 experiments of each algorithm

--- _examples/original/_run_experiments.bat
+++ _examples/original/_run_experiments.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set ITERATIONS=20
+set ITERATIONS=25
 
 REM first run just to get some history
 python dagforpredictions_expensive_computations.py simple
@@ -9,19 +9,19 @@ timeout /t 2 /nobreak
 
 REM Actually run the loops
 for /L %%i in (1,1,%ITERATIONS%) do (
-    echo Running first iteration %%i
+    echo Running SIMPLE algorithm %%i
     python dagforpredictions_expensive_computations.py simple
     timeout /t 2 /nobreak
 )
 
 for /L %%j in (1,1,%ITERATIONS%) do (
-    echo Running second iteration %%j
+    echo Running FIRST algorithm %%j
     python dagforpredictions_expensive_computations.py first
     timeout /t 2 /nobreak
 )
 
 for /L %%j in (1,1,%ITERATIONS%) do (
-    echo Running second iteration %%j
+    echo Running SECOND algorithm %%j
     python dagforpredictions_expensive_computations.py second
     timeout /t 2 /nobreak
 )
