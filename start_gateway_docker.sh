6da04010288d9ac0cd50cdde31c857cc2588c21b
Diogo Jesus <plopydasgaming@gmail.com>
2025-09-26 13:20:53 +0100

    udpate scripts

--- start_gateway_docker.sh
+++ start_gateway_docker.sh
@@ -3,7 +3,6 @@
 # expose the Docker REST API
 sudo dockerd -H unix:///var/run/docker.sock -H tcp://127.0.0.1:2375 &
 
-source venv/bin/activate
 export DOCKER_IMAGE=docker_worker
 export TZ=UTC
 waitress-serve --port=5000 src.docker_workers_gateway.handler:app
