2d95ddee496f783f35999eeca4e700db25cbfc80
Diogo Jesus <plopydasgaming@gmail.com>
2025-10-07 20:28:06 +0100

    a

--- _examples/original/_run_experiments.py
+++ _examples/original/_run_experiments.py
@@ -5,13 +5,13 @@ import subprocess
 import requests
 
 WORKFLOWS_PATHS = [
-    # 'gemm.py',
-    # 'tree_reduction.py',
+    'gemm.py',
+    'tree_reduction.py',
     'image_transformer.py',
     'text_analysis.py',
 ]
 
-ITERATIONS_PER_ALGORITHM = 6
+ITERATIONS_PER_ALGORITHM = 5
 ALGORITHMS = ['uniform', 'non-uniform']
 # ALGORITHMS = ['wukong', 'wukong-opt', 'uniform', 'uniform-opt', 'non-uniform', 'non-uniform-opt']
 # ALGORITHMS = ['uniform']
