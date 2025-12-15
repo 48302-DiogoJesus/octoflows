4789b0e274c23fa65398900fbcff9eddbb5fa75a
Diogo Jesus <dijesus3@gmail.com>
2025-12-15 19:06:29 +0000

    a

--- src/storage/redis_storage.py
+++ src/storage/redis_storage.py
@@ -29,7 +29,7 @@ class RedisStorage(storage.Storage):
     class Config(storage.Storage.Config):
         address: tuple[str, int]
         password: str
-        max_concurrent_heavy_ops: int = 30
+        max_concurrent_heavy_ops: int = 15
 
         def create_instance(self) -> "RedisStorage":
             return RedisStorage(self)
