2b783f23bda1c34a1123b5b409752e0376963fdc
Diogo Jesus <dijesus3@gmail.com>
2025-12-18 17:49:09 +0000

    a

--- _examples/original/_run_experiments.py
+++ _examples/original/_run_experiments.py
@@ -14,8 +14,8 @@ WORKFLOWS_PATHS = [
 # ITERATIONS_PER_ALGORITHM = 10
 ITERATIONS_PER_ALGORITHM = 2
 # ALGORITHMS = ['uniform', 'non-uniform']
-# ALGORITHMS = ['wukong', 'wukong-opt', 'uniform', 'uniform-opt', 'non-uniform', 'non-uniform-opt']
-ALGORITHMS = ['non-uniform', 'non-uniform-opt']
+ALGORITHMS = ['wukong', 'wukong-opt', 'uniform', 'uniform-opt', 'non-uniform', 'non-uniform-opt']
+# ALGORITHMS = ['non-uniform', 'non-uniform-opt']
 SLAS = ['50']
 # SLAS = ['50', '75', '90']
 
