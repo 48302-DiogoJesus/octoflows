9ab2d5201f362cb35d93f642cf634d372def80e1
Diogo Jesus <dijesus3@gmail.com>
2025-12-21 04:09:39 +0000

    a

--- src/docker_workers_gateway/container_pool_executor.py
+++ src/docker_workers_gateway/container_pool_executor.py
@@ -11,7 +11,7 @@ from src.docker_worker_handler.worker import ATOMIC_FILE_FOR_WARM_START_DETECTIO
 
 logger = create_logger(__name__)
 ALLOW_CONTAINER_REUSAGE = True
-TIME_UNTIL_WORKER_GOES_COLD_S = 8
+TIME_UNTIL_WORKER_GOES_COLD_S = 7
 
 @dataclass
 class Container:
