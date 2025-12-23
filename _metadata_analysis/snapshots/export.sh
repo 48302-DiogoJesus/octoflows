179b04559fd5c1829a04d480b9d8b510e901d3fd
Diogo Jesus <dijesus3@gmail.com>
2025-12-23 12:21:27 +0000

    a

--- _metadata_analysis/snapshots/export.sh
+++ _metadata_analysis/snapshots/export.sh
@@ -1,4 +1,4 @@
 docker exec metrics-storage-redis \
-  redis-cli -h 10.15.0.22 -a "$REDIS_PASSWORD" SAVE
+  redis-cli -h 10.15.0.22 -a "redisdevpwd123" SAVE
 
 docker cp metrics-storage-redis:/data/dump.rdb ./metrics_backup.rdb
